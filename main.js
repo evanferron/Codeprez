d596fc143c443ded09618d5754867a5fe6b9e1b3
evan <evan.ferron53@gmail.com>
2025-06-23 14:07:14 +0200

    add function to use to unzip files

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
 const fs = require('fs');
-const JSZip = require('jszip');
 const path = require('path');
+const AdmZip = require('adm-zip');
 
 
-const zipFile = (path) => {
+const zipFile = () => {
 
+    const zip = new AdmZip();
+
+    zip.addLocalFile(path.join(__dirname, 'data.json'));
+    zip.addLocalFile(path.join(__dirname, 'presentation.md'));
+
+    zip.writeZip(path.join(__dirname, 'test.zip'))
+    fs.rename(path.join(__dirname, 'test.zip'), path.join(__dirname, 'test.codeprez'), (err) => {
+        if (err) {
+            console.error('Error renaming file:', err);
+        } else {
+            console.log('File renamed successfully to test.codeprez');
+        }
+    });
 }
+
+const unzipFile = () => {
+    const zip = new AdmZip(path.join(__dirname, 'test.codeprez'));
+
+    zip.extractAllTo(path.join(__dirname, "test"), true);
+    console.log('Files extracted successfully');
+}
+
+
+unzipFile();
